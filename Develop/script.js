59071b45ffba4d3b6a49228819936889765e5782
Sm4ck <mikeamartinez314@gmail.com>
2022-05-25 17:57:22 -0500

    created function for password generation and a prompt for password character length

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,28 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Create a function with all possible characters
+function generatePassword() {
+  var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "`", "~", "-", "_", "=", "+", "[", "]", "{", "}", "\\", "|", ";", ":", ",", ".", "/", "<", ">", "?", "'"];
+}
+
+// Ask User to specify a password length
+var passwordLength = prompt ("Please choose a password length between 8 and 128 characters.")
+
+if (length < 8 || length > 128) {
+  alert("You must choose a password length between 8 and 128 characters!");
+}
+
+// Prompting User for password criteria
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
